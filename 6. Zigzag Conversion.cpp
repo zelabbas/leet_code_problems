51ce02058a8311b3659104fead18be5985331c1c
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-06 23:01:42 +0100

    new problem done

---
+++ 6. Zigzag Conversion.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string convert(string s, int numRows)
+    {
+        if (numRows == 1)
+            return s;
+        int value = 0;
+        string str;
+        int x = 1;
+        int size = s.size();
+        string tab_str[numRows];
+        for(size_t i = 0; i < size; i++)
+        {
+            tab_str[value] += s[i];
+            if (value == (numRows - 1) || value == 0 && i != 0)
+                x *= -1;
+            value += x;
+        }
+        for(size_t j= 0; j < numRows; j++)
+            str +=tab_str[j];
+        return str;
+    }
+};
