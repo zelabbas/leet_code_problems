575aed6c818ebf6c9a1a9800e2be7fed42de4a09
zelabbas <zakariaelabbassi2002@gmail.com>
2024-06-03 15:39:30 +0100

    solve new problem from leet code

---
+++  Code Testcase Test Result Test Result 345. Reverse Vowels of a String.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 15:38:56 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/06/03 15:39:00 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+class Solution {
+public:
+
+	bool isVowels(char c)
+	{
+		if ((c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'))
+			return (true);
+		else if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+			return (true);
+		return (false);
+	}
+    string reverseVowels(string s) {
+		int i = 0;
+		int len = s.length() - 1;
+		while (i < len)
+		{
+			
+			if (isVowels(s[i]) && isVowels(s[len]))
+			{
+				s[i] ^= s[len];
+				s[len] ^= s[i];
+				s[i] ^= s[len];
+				i++;
+				len--;
+			}
+            else if (!isVowels(s[i]))
+				i++;
+			else if (!isVowels(s[len]))
+				len--;
+		}
+		return (s);
+    }
+};
