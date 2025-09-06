153c56afbecc585d62ccd2a6afde0f43c58bb9b6
zelabbas <zakariaelabbassi2002@gmail.com>
2025-09-06 20:56:57 +0100

    new problem at leetcode

---
+++ VersionCpp/1486. XOR Operation in an Array.cpp
@@ -0,0 +1,14 @@
+// 1486. XOR Operation in an Array
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum ^= (start + 2 * i);
+        }
+
+        return sum;
+    }
+};
